ea06cc313b304b3e91e1bff539a58e306cf20168
AdithKP000 <adithkp007@gmail.com>
2025-04-10 20:25:20 +0530

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -648,12 +648,7 @@ export const uploadImage = async (req, res) => {
     export const getAllUserController = async(req,res)=>{
         try {
                 const users= await userModel.find()
-<<<<<<< HEAD
                  .select("-image -otp -otpExpires -resertPassword -resetPasswordOtpExpires -approvalStatus")
-=======
-                 .select("-image -otp -otpExpires -resertPassword -resetPasswordOtpExpires")
->>>>>>> 813c4aabb379c8fcf6aa4ae1aa57ca1dc33b057f
-            
                 if(!users){
                     console.log("Unable to fetch all")
                 }
